c8224924ef5d08eb85711cf4a81ce8a517f7b025
AkhmadRifaldi <akhmadrifaldi25@gmail.com>
2023-10-25 15:22:18 +0800

    Quiz

---
+++ bin/Generic.dart/bounded_type_parameter.dart
@@ -0,0 +1,6 @@
+import 'data/bounded_type_parameter.dart';
+
+void main() {
+  // var dataString = NumberData("Eko"); // error
+  var dataInt = NumberData(10);
+}
---
+++ bin/Generic.dart/generic_function.dart
@@ -0,0 +1,9 @@
+import "data/generic_function.dart";
+
+void main (){
+  var number = [1, 2, 3, 4, 6,];
+  var Names = ["Eko", "Kurniawan", "Khannedy"];
+
+  print(ArrayHelper.count(number));
+  print((ArrayHelper.count));
+}
